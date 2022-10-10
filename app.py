91c485dae1eb12dce1c4af40b86eade78ea8c34d
nispar2009 <varun.parupalli@gmail.com>
2022-10-10 11:55:23 +0530

    Home page set up

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
+from crypt import methods
 from flask import Flask, redirect, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
 # Establishing connection
-connection = sqlite3.connect("rockpaperscissors.db")
+connection = sqlite3.connect('rockpaperscissors.db')
 cursor = connection.cursor()
 
 # Creating tables
-cursor.execute("CREATE TABLE IF NOT EXISTS users(username TEXT UNIQUE, onlineWins INT, computerWins INT)")
-cursor.execute("CREATE TABLE IF NOT EXISTS challenges(user1 TEXT, user2 TEXT, input1 TEXT, input2 TEXT, winner TEXT)")
+cursor.execute('CREATE TABLE IF NOT EXISTS users(username TEXT UNIQUE, onlineWins INT, computerWins INT)')
+cursor.execute('CREATE TABLE IF NOT EXISTS challenges(id INTEGER PRIMARY KEY, user1 TEXT, user2 TEXT, input1 TEXT, input2 TEXT, winner TEXT)')
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    connection = sqlite3.connect('rockpaperscissors.db')
 
-if __name__ == "__main__":
+    user = request.args.get('user')
+
+    if user:
+        user_in_db = cursor.execute('SELECT * FROM users WHERE username=?', (user,))
+        
+        if user_in_db:
+            return render_template('index.html', user=user_in_db)
+
+        # Signing up
+
+        cursor.execute('INSERT INTO users VALUES (?, 0, 0)', (user,))
+        connection.commit()
+
+        new_user = cursor.execute('SELECT * FROM users WHERE username=?', (user,))
+
+        connection.close()
+
+        return render_template('index.html', user=new_user)
+
+    # Signing in
+
+    return render_template('signin.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
